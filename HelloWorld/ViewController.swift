a32ef5abc681a5a19b75b302ee1ec92ade2ef9ac
Noah Franck <ultimatedeath11@icloud.com>
2018-09-13 01:15:14 -0500

    added basic functionality for the switching of colors and rotation their values based on modulus. Both have 3 options.

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var TitleTextField: UITextField!
+    
+    @IBOutlet var mainview: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TitleTextField.textColor = UIColor.black
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    var incramenter: integer_t = 0
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onPressButton(_ sender: UIButton)
+    {
+        incramenter += 1
+        if incramenter % 1 == 0
+        {
+            TitleTextField.textColor = UIColor.red
+        }
+        if incramenter % 2 == 0
+        {
+            TitleTextField.textColor = UIColor.cyan
+        }
+        if incramenter % 3 == 0
+        {
+            TitleTextField.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func backgroundButton(_ sender: UIButton)
+    {
+        incramenter += 1
+        if incramenter % 1 == 0
+        {
+            mainview.backgroundColor = UIColor.green
+        }
+        if incramenter % 2 == 0
+        {
+            mainview.backgroundColor = UIColor.blue
+        }
+        if incramenter % 3 == 0
+        {
+            mainview.backgroundColor = UIColor.magenta
+        }
+    }
 }
 
